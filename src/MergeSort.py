a5cd8ed510dc6f257ec9003920c2e93d3259f110
BozeBro <62128128+BozeBro@users.noreply.github.com>
2020-05-08 16:16:28 -0500

    Update MergeSort.py

--- src/MergeSort.py
+++ src/MergeSort.py
@@ -3,48 +3,54 @@ from matplotlib.animation import FuncAnimation
 from random import shuffle
 
 
-def mergesort2(A, start, end):
-    # This function does not use splices but maintains original list
-
+def mergesort2(array, start, end):
     if end <= start:
         return
 
     mid = start + ((end - start + 1) // 2) - 1
-    yield from mergesort2(A, start, mid)
-    yield from mergesort2(A, mid + 1, end)
-    yield from merge2(A, start, mid, end)
-    if start == 0 and end == len(A) - 1:
+    yield from mergesort2(array, start, mid)
+    yield from mergesort2(array, mid + 1, end)
+    yield from merge2(array, start, mid, end)
+    if start == 0 and end == len(array) - 1:
         yield 0, 0, 1
 
 
-def merge2(A, start, mid, end):
-    merged = []
-    leftIdx = start
-    rightIdx = mid + 1
+def merge2(array, start, mid, end):
+    merged, left, right = [], start, mid + 1
 
-    while leftIdx <= mid and rightIdx <= end:
-        if A[leftIdx] < A[rightIdx]:
-            merged.append(A[leftIdx])
-            leftIdx += 1
+    while left <= mid and right <= end:
+        if array[left] < array[right]:
+            merged.append(array[left])
+            yield left, array[left], 0
+            left += 1
         else:
-            merged.append(A[rightIdx])
-            rightIdx += 1
+            merged.append(array[right])
+            yield right, array[right], 2
+            right += 1
 
-    while leftIdx <= mid:
-        merged.append(A[leftIdx])
-        leftIdx += 1
+    while left <= mid:
+        merged.append(array[left])
+        yield left, array[left], 0
+        left += 1
 
-    while rightIdx <= end:
-        merged.append(A[rightIdx])
-        rightIdx += 1
+    while right <= end:
+        merged.append(array[right])
+        yield right, array[right], 2
+        right += 1
 
     for i, sorted_val in enumerate(merged):
-        A[start + i] = sorted_val
+        array[start + i] = sorted_val
         yield start + i, sorted_val, 0
 
 
-def update(array):
-    i, j, c = array
+def update(container):
+    i, j, c = container
+    if c == 2:
+        if storage['store2'] != None:
+            wedges[storage['store2']].set_color(storage['store3'])
+        wedges[i].set_color('black')
+        storage['store2'], storage['store3'] = i, storage[j]
+        return
     if storage['store0'] != None:
         wedges[storage['store0']].set_color(storage['store1'])
     if c == 0:
@@ -70,9 +76,11 @@ wedges, texts = ax.pie(to_sort, radius=4, startangle=90., shadow=True,
                        wedgeprops={'linewidth': False,
                                    'linestyle': 'solid'},
                        colors=test)
-
 storage = {n: c for n, c in zip(length, test)}
-storage['store0'], storage['store1'] = None, None
+storage['store0'], storage['store1'], \
+storage['store2'], storage['store3'] = None, None, \
+                                       None, None
+
 # Storage to remember to switch off black marker
 ax.axis('equal')
 ani = FuncAnimation(fig, update, frames=mergesort2(length, 0, len(length) - 1), repeat=False, interval=1,
