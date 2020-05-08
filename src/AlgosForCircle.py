ee6ec0b7b16c8dbf1077d0a41ca1eb942c5093a8
BozeBro <62128128+BozeBro@users.noreply.github.com>
2020-05-08 11:36:42 -0500

    Update AlgosForCircle.py

--- src/AlgosForCircle.py
+++ src/AlgosForCircle.py
@@ -38,36 +38,3 @@ def selectionSort(array):
         # upgrade i so the starting point is moved
     yield 0, 0, 1
 
-def quickSort(array, bot, top):
-    """This will do the quickSort sort"""
-    if bot < top:
-        index_p = randint(bot, top)
-        pivot = array[index_p]
-        array[index_p], array[top] = array[top], array[index_p]
-        yield index_p, top, 0
-        i = bot
-        i2 = top - 1
-        while True:
-            while array[i] <= pivot and i < i2:
-                # This should get the number greater than pivot
-                i += 1
-            while array[i2] > pivot and i2 > i and i2 > bot:
-                # This should get the number less than the pivot
-                i2 -= 1
-            if i >= i2:
-                if array[i] > array[top]:
-                    array[i], array[top] = array[top], array[i]
-                    yield i, top, 0
-                    pi = i
-                    break
-                elif array[i] <= array[top]:
-                    pi = top
-                    break
-            elif array[i] > pivot >= array[i2]:
-                array[i2], array[i] = array[i], array[i2]
-                yield i, i2, 0
-        yield from quickSort(array, bot, pi - 1)
-        yield from quickSort(array, pi + 1, top)
-        if bot == 0 and top == len(array)-1:
-            yield 0, 0, 1
-
